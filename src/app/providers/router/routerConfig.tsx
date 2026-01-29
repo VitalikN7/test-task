1c802e218718f2130459876ed935cceb728bc8dc
Vitalik <xsylarxn7@gmail.com>
2026-01-29 10:24:03 +0300

    fix deploy_1

--- src/app/providers/router/routerConfig.tsx
+++ src/app/providers/router/routerConfig.tsx
@@ -1,11 +1,11 @@
 //* router
-import { createBrowserRouter, redirect } from "react-router";
+import { createHashRouter, redirect } from "react-router";
 //* pages
 import { RequestsListPage } from "pages/RequestsListPage";
 import { CreateRequestPage } from "pages/CreateRequestPage";
 import { RequestDetailsPage } from "pages/RequestDetailsPage";
 //* все роуты приложения
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     loader: () => redirect("/requests"),
